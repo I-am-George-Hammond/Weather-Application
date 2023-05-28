4b977b4c47ad320d239612427385fb9b11845680
Leander-Botha <578404@student.belgiumcampus.ac.za>
2023-05-28 17:19:36 +0200

    Import API, search

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from 'react';
+
+const API_URL = "http://api.openweathermap.org/data/2.5/weather?zip=0182,za&appid=298a3ab3b55539f0398ba22e87a4433b";
+const secretKey = "298a3ab3b55539f0398ba22e87a4433b"
+
+
 function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchWeather = async (zipCode) => {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+
+    setWeatherInfo(data.weather)
+  }
+  useEffect(()=> {
+    searchWeather()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  <div> 
+
+    <h1>Weather App</h1>
+
+    <div className = "searchZipCode">
+      <input placeholder = "Enter the zip code " value = {searchTerm} onChange = {(e)=> setSearchTerm(e.target.value)}></input>
+      <button onClick = {()=> {}}>Search</button>
     </div>
+
+    <div className = "container">
+      <div className = "Weather">
+        <p>weatherInfo</p>
+
+        {
+            weatherInfo.map((item)=> (
+                <p>item</p>
+
+            ))}
+
+
+      </div>
+    </div>
+
+  </div>    
+
   );
 }
 
