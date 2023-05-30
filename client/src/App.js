0a9df7f09dc2637f0076d181c63f16d4f676108a
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2023-05-30 20:51:26 +0200

    UI gebou

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./App.css"
+import searchIcon  from "./Images/search.png"
+import weatherIcon from "./Images/rain.png"
 
 const App = () => {
   const [zipcode, setZipcode] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    document.title = 'Weather App'; 
+  }, []);
 
   const handleZipcodeChange = event => {
     setZipcode(event.target.value);
@@ -30,25 +37,42 @@ const App = () => {
   };
 
   return (
-    <div>
+      <>
+    <div className="Container" >
       <h1>Weather App</h1>
-      <form onSubmit={handleSubmit}>
+      <form className="search" onSubmit={handleSubmit}>
         <input type="text" value={zipcode} onChange={handleZipcodeChange} placeholder="Enter ZIP code" />
-        <button type="submit">Get Weather</button>
+        <button type="submit"> <img src={searchIcon} alt="Search button"></img> </button>
       </form>
+
+      {/* test purpose vir ui bou --- delete--- */}
+      
+      <div className='cardWeather'>
+          <img src={weatherIcon} ></img>
+          <h1>26&deg;c</h1>
+          <h2>Shongololo</h2>
+          <br></br>
+          <p>Min: 7&deg;c </p>
+          <p>Max: 27&deg;c </p>
+          <p>Wind Speed: 15km/h </p>
+          <p>Humidity: 50% </p>
+        </div>
+     
 
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
 
       {weatherData && (
-        <div>
-          <p>Min Temperature: {weatherData.minTemp}</p>
-          <p>Max Temperature: {weatherData.maxTemp}</p>
-          <p>Current Temperature: {weatherData.currentTemp}</p>
-          <p>Wind Speed: {weatherData.windSpeed}</p>
-        </div>
+        <div className='cardWeather'>
+        <p>Min Temperature: {weatherData.minTemp}</p>
+        <p>Max Temperature: {weatherData.maxTemp}</p>
+        <p>Current Temperature: {weatherData.currentTemp}</p>
+        <p>Wind Speed: {weatherData.windSpeed}</p>
+      </div>
       )}
+    
     </div>
+    </>
   );
 };
 
