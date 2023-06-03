17291779b547b050728864d40c09c045b2ebda18
Hanu Engelbrecht <engelbrecht.hanu@gmail.com>
2023-06-03 15:33:15 +0200

    Last Component update
    Updating Server.js files

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-// Express module import 
 const express = require('express');
 const app = express();
-const axios = require('axios')
-// importing body parser module
+const axios = require('axios');
 const bodyParser = require('body-parser');
 
-// port number to be used 
 const port = 5000;
 
-//API url and API key.
+const API_BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '298a3ab3b55539f0398ba22e87a4433b';
 
-const API_URL = "http://api.openweathermap.org/data/2.5/weather?zip=0182,za&appid=298a3ab3b55539f0398ba22e87a4433b";
-const secretKey = "298a3ab3b55539f0398ba22e87a4433b";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.get('/api', (req, res) => {
+  const { zipCode } = req.query;
+  const apiUrl = `${API_BASE_URL}?zip=${zipCode},za&appid=${API_KEY}`;
 
-// Responding to a web request.
-app.get("/api", (req,res)=> {
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error while fetching weather data');
+    });
+});
 
-        axios.get(API_URL)
-        .then(response => {
-        res.json(response.data)
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
 
-        })
-        .catch(error => {
-                console.error(error)
-                res.status(500).send('Error while fetching weather data')
-         })
-})
-
-//Starting the server.
-app.listen(port, ()=> {
-        console.log(`App listening at http://localhost:${port}`)
-})
